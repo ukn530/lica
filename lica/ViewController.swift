3f2995b9a9558dfd527a3a9d6ad633f224eb24b1
ukn530 <ukn530@gmail.com>
2015-05-22 23:07:01 +0900

    Initial Commit

---
+++ lica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lica
+//
+//  Created by ukn on 5/22/15.
+//  Copyright (c) 2015 ukn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
